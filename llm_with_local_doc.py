ddf095ca0c81549b51ea08e4526b2ce11b11fd10
richard <whoami@hotmail.com>
2025-03-14 15:47:24 +0800

    debug

--- llm_with_local_doc.py
+++ llm_with_local_doc.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_ollama import OllamaLLM
 
 import logging.config
-import httpx
 
 # 加载配置
 logging.config.fileConfig('logging.conf')
@@ -22,8 +22,8 @@ def req_with_vector_db(question: str) -> str:
     加载本地矢量数据库文件, 调用 LLM API, 进行 RAG, 输出结果
     """
     embedding_model = "../bge-large-zh-v1.5"
-    # api_url = "http://127.0.0.1:11435"
-
+    api_url = "http://127.0.0.1:11434"
+    api_key = "12345"
     faiss_index = "./faiss_index"
 
     # llm_name = "llama3.1:8b"
@@ -43,22 +43,30 @@ def req_with_vector_db(question: str) -> str:
     logger.info("get remote llm agent")
     # 可调用无需 api_key 的model
     llm = OllamaLLM(model=llm_name, base_url=api_url)
-
+    doc = loaded_index.similarity_search_with_relevance_scores(question)
+    [documents[i] for i in doc[0]]
+    logger.info("retrieved doc is: ".format(doc))
     # 调用需要 api_key 的 model
-    llm = ChatOpenAI(api_key=api_key, base_url=api_url, http_client=httpx.Client(verify=False), model=remote_llm_name)
+    # llm = ChatOpenAI(api_key=api_key, base_url=api_url, http_client=httpx.Client(verify=False), model=remote_llm_name)
     # 创建检索问答链
     logger.info("build retrieval")
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=loaded_index.as_retriever())
-
-    # qa = RetrievalQA.from_chain_type(
-    #     llm=llm,
-    #     chain_type="map_reduce",
-    #     retriever=loaded_index.as_retriever(search_kwargs={"k": 2, "score_threshold": 0.7}),
-    #     return_source_documents=True
-    # )
-    # 提问
+    system_prompt = (
+        "根据提供的背景信息回答问题."
+        "如果你不知道答案，就说你不知道."
+        "回答内容要精炼，最多给出三句话."
+        "Context: {context}"
+    )
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    chain = create_retrieval_chain(doc.as_retriever(), question_answer_chain)
     logger.info("invoke retrieval {} to uri {}".format(question, api_url))
-    result = qa.invoke(question)
+    result = chain.invoke({"input": question})
+    # 提问
     answer = result["result"]
     logger.info("answer is {}".format(answer))
     return answer
