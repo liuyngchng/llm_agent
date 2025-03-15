bebb647349858d3aff8fae2f2a6febdd395c8534
richard <whoami@hotmail.com>
2025-03-15 09:00:34 +0800

    debug

--- llm_with_local_doc.py
+++ llm_with_local_doc.py
@@ -1,73 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
+
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_ollama import OllamaLLM
+from langchain_ollama import ChatOllama
+from semantic_search import get_vector_db
 
 import logging.config
 
-# 加载配置
 logging.config.fileConfig('logging.conf')
-
-# 创建 logger
 logger = logging.getLogger(__name__)
+model_name = "deepseek-r1:7b"
+api_url = "http://127.0.0.1:11434"
 
 
 def req_with_vector_db(question: str) -> str:
-    """
-    加载本地矢量数据库文件, 调用 LLM API, 进行 RAG, 输出结果
-    """
-    embedding_model = "../bge-large-zh-v1.5"
-    api_url = "http://127.0.0.1:11434"
-    api_key = "12345"
-    faiss_index = "./faiss_index"
-
-    # llm_name = "llama3.1:8b"
-    # llm_name = "llama3.2:3b-text-q5_K_M"
-    llm_name = "deepseek-r1:7b"
-    remote_llm_name = "deepseek-r1"
-    # for test purpose only, read index from local file
-    logger.info("embedding_model: {}".format(embedding_model))
-    embeddings = HuggingFaceEmbeddings(model_name=embedding_model, cache_folder='./bge-cache')
-    logger.info("try to load index from local file")
-    loaded_index = FAISS.load_local(faiss_index, embeddings,
-                                    allow_dangerous_deserialization=True)
-    # loaded_index = SQLiteVSS.create_connection(db_file="/sqlite/vss.db")
     logger.info("load index from local file finish")
+    docs_with_scores = get_vector_db().similarity_search_with_relevance_scores(question, k=2)
 
-    # 创建远程 Ollama API代理
-    logger.info("get remote llm agent")
-    # 可调用无需 api_key 的model
-    llm = OllamaLLM(model=llm_name, base_url=api_url)
-    doc = loaded_index.similarity_search_with_relevance_scores(question)
-    [documents[i] for i in doc[0]]
-    logger.info("retrieved doc is: ".format(doc))
-    # 调用需要 api_key 的 model
-    # llm = ChatOpenAI(api_key=api_key, base_url=api_url, http_client=httpx.Client(verify=False), model=remote_llm_name)
-    # 创建检索问答链
-    logger.info("build retrieval")
-    system_prompt = (
-        "根据提供的背景信息回答问题."
-        "如果你不知道答案，就说你不知道."
-        "回答内容要精炼，最多给出三句话."
-        "Context: {context}"
-    )
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", system_prompt),
-            ("human", "{input}"),
-        ]
-    )
-    question_answer_chain = create_stuff_documents_chain(llm, prompt)
-    chain = create_retrieval_chain(doc.as_retriever(), question_answer_chain)
-    logger.info("invoke retrieval {} to uri {}".format(question, api_url))
-    result = chain.invoke({"input": question})
+    # 输出结果和相关性分数
+    # for doc, score in docs_with_scores:
+    #     print(f"[相关度：{score:.2f}] {doc.page_content[:200]}...")
+    # 构建增强提示
+    template = """基于以下上下文：
+            {context}
+            
+            回答：{question}"""
+    prompt = ChatPromptTemplate.from_template(template)
+
+    model = ChatOllama(model=model_name, base_url=api_url)
+    chain = prompt | model
+    logger.info("submit user question in LLM: {}".format(question))
+    response = chain.invoke({
+        "context": "\n\n".join([doc.page_content for doc, score in docs_with_scores]),
+        "question": question
+    })
     # 提问
-    answer = result["result"]
+    answer = response.content
     logger.info("answer is {}".format(answer))
     return answer
 
