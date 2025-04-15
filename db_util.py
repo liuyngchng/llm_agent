a6b14cef325030d963bf08c42525d93f83081c98
richard <liuyngchng@hotmail.com>
2025-04-15 10:31:36 +0800

    debug

--- db_util.py
+++ db_util.py
@@ -6,7 +6,7 @@ import sqlite3
 import json
 import pandas as pd
 import logging.config
-# import cx_Oracle
+
 
 from urllib.parse import urlparse, unquote, urlencode
 from sys_init import init_yml_cfg
@@ -25,18 +25,6 @@ Oracle 数据库需要额外安装其客户端动态库
 
 logging.config.fileConfig('logging.conf', encoding="utf-8")
 logger = logging.getLogger(__name__)
-
-
-# def oracle_query_tool(db_con, query: str) -> str:
-#     try:
-#         cursor = db_con.cursor()
-#         cursor.execute(query)
-#         columns = [desc[0] for desc in cursor.description] if cursor.description else []
-#         data = cursor.fetchall()
-#         return json.dumps({"columns": columns, "data": data}, ensure_ascii=False)
-#     except Exception as e:
-#         logger.error(f"oracle_query_tool_err: {e}")
-#         raise e
 
 def mysql_query_tool(db_con, query: str) -> str:
     try:
@@ -148,40 +136,56 @@ def mysql_output(cfg: dict, sql:str, data_format:str):
     my_conn.close()
     return dt
 
-# def oracle_output(cfg: dict, sql: str, data_format: str):
-#     """
-#     cfg['db']['uri'] = oracle+cx_oracle://user:password@host:port/service_name
-#     """
-#     db_config = cfg.get('db', {})
-#     if all(key in db_config for key in ['name', 'host', 'user', 'password']):
-#         dsn = cx_Oracle.makedsn(
-#             db_config['host'],
-#             db_config.get('port', 1521),
-#             service_name=db_config['name']
-#         )
-#         conn = cx_Oracle.connect(
-#             user=db_config['user'],
-#             password=db_config['password'],
-#             dsn=dsn
-#         )
-#     else:
-#         parsed_uri = urlparse(db_config['uri'])
-#         port = parsed_uri.port or 1521
-#         dsn = cx_Oracle.makedsn(
-#             unquote(parsed_uri.hostname),
-#             port,
-#             service_name=unquote(parsed_uri.path[1:])
-#         )
-#         conn = cx_Oracle.connect(
-#             user=unquote(parsed_uri.username),
-#             password=unquote(parsed_uri.password),
-#             dsn=dsn
-#         )
-#
-#     dt = output_data(conn, sql, data_format)
-#     conn.close()
-#     return dt
-#
+#################### for support oracle DB #########################
+import cx_Oracle
+def oracle_query_tool(db_con, query: str) -> str:
+    try:
+        cursor = db_con.cursor()
+        cursor.execute(query)
+        columns = [desc[0] for desc in cursor.description] if cursor.description else []
+        data = cursor.fetchall()
+        return json.dumps({"columns": columns, "data": data}, ensure_ascii=False)
+    except Exception as e:
+        logger.error(f"oracle_query_tool_err: {e}")
+        raise e
+
+def oracle_output(cfg: dict, sql: str, data_format: str):
+    """
+    cfg['db']['uri'] = oracle+cx_oracle://user:password@host:port/service_name
+    """
+    db_config = cfg.get('db', {})
+    if all(key in db_config for key in ['name', 'host', 'user', 'password']):
+        dsn = cx_Oracle.makedsn(
+            db_config['host'],
+            db_config.get('port', 1521),
+            service_name=db_config['name']
+        )
+        conn = cx_Oracle.connect(
+            user=db_config['user'],
+            password=db_config['password'],
+            dsn=dsn
+        )
+    else:
+        parsed_uri = urlparse(db_config['uri'])
+        port = parsed_uri.port or 1521
+        dsn = cx_Oracle.makedsn(
+            unquote(parsed_uri.hostname),
+            port,
+            service_name=unquote(parsed_uri.path[1:])
+        )
+        conn = cx_Oracle.connect(
+            user=unquote(parsed_uri.username),
+            password=unquote(parsed_uri.password),
+            dsn=dsn
+        )
+
+    dt = output_data(conn, sql, data_format)
+    conn.close()
+    return dt
+
+#################### for support oracle DB #########################
+
+
 def sqlite_output(db_uri: str, sql:str, data_format:str):
     """
     cfg["db_uri"] = "sqlite:///test1.db"
@@ -207,9 +211,9 @@ def get_db_uri(cfg: dict) -> str:
                          f"@{db_config['host']}:{db_config.get('port', 3306)}/{db_config['name']}")
         elif 'sqlite' in db_type_cfg:
             my_db_uri = f"sqlite:///{db_config['name']}"
-        # elif 'oracle' in db_type_cfg:
-        #     my_db_uri = (f"oracle+cx_oracle://{db_config['user']}:{db_config['password']}"
-        #                  f"@{db_config['host']}:{db_config.get('port', 1521)}/?service_name={db_config['name']}")
+        elif 'oracle' in db_type_cfg:
+            my_db_uri = (f"oracle+cx_oracle://{db_config['user']}:{db_config['password']}"
+                         f"@{db_config['host']}:{db_config.get('port', 1521)}/?service_name={db_config['name']}")
         else:
             raise "unknown db type in config file"
     else:
