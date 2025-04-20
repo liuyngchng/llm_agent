7eb8a509a9ae1e87e504233fadd59dd480fa6c60
richard <liuyngchng@hotmail.com>
2025-04-20 11:24:29 +0800

    debug

--- build_nl2sql.sh
+++ build_nl2sql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --rm -f ./Dockerfile_nl2sql ./ -t llm_nl2sql:1.1
-docker images | grep '<none>' | awk -F ' ' '{print $3}' | xargs docker rmi -r
+docker images | grep '<none>' | awk -F ' ' '{print $3}' | xargs docker rmi -f
 docker images
--- build_py_env.sh
+++ build_py_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --rm -f ./Dockerfile_py ./ -t ubuntu_py:24.04
-docker images | grep '<none>' | awk -F ' ' '{print $3}' | xargs docker rmi -r
+docker images | grep '<none>' | awk -F ' ' '{print $3}' | xargs docker rmi -f
 docker images
