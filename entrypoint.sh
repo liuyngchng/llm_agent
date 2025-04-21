1a19f35bfd553b654759c790e6febcbce8fa902b
richard <liuyngchng@hotmail.com>
2025-04-21 16:18:21 +0800

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 /bin/bash -c 'source ../llm_py_env/bin/activate'
+#MODULE=http_nl2sql
+MODULE=http_rag
 echo "current dir `pwd`"
 #nohup /opt/llm_py_env/bin/gunicorn --timeout 240 --preload -w 4 -b 0.0.0.0:19000 http_service:app > server.log &2>&1 &
-
-
 # just for simplicity to deploy and share memory
-/opt/llm_py_env/bin/gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 http_nl2sql:app
+/opt/llm_py_env/bin/gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
 # real production environment
-/opt/llm_py_env/bin/gunicorn --timeout 240 --preload -w 4 -b 0.0.0.0:19000 http_nl2sql:app
+#/opt/llm_py_env/bin/gunicorn --timeout 240 --preload -w 4 -b 0.0.0.0:19000 ${MODULE}:app
--- start_rag.sh
+++
@@ -1,8 +0,0 @@
-/bin/bash -c 'source ../llm_py_env/bin/activate'
-echo "current dir `pwd`"
-#nohup /opt/llm_py_env/bin/gunicorn --timeout 240 --preload -w 4 -b 0.0.0.0:19000 http_service:app > server.log &2>&1 &
-
-# just for simplicity to deploy and share memory
-/opt/llm_py_env/bin/gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 http_rag:app
-# real production environment
-#/opt/llm_py_env/bin/gunicorn --timeout 240 --preload -w 4 -b 0.0.0.0:19000 http_rag:app
