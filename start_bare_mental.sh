9f4ab4c15141e82c9c19a119ebc97e5def9fc96e
richard <liuyngchng@hotmail.com>
2025-04-21 14:42:12 +0800

    debug

--- start_bare_mental.sh
+++ start_bare_mental.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
+MODULE=http_rag
+#MODULE=http_nl2sql
+echo 'starting module '${MODULE}
 /bin/bash -c 'source ../llm_py_env/bin/activate'
-../llm_py_env/bin/gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 http_rag:app
+../llm_py_env/bin/gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
+echo "module ${MODULE} stared"
