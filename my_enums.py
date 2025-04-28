cc2a4ddd8263dac8c96d1bbbdb9914ea2f3f760f
richard <whoami@hotmail.com>
2025-04-28 16:53:17 +0800

    debug

--- my_enums.py
+++ my_enums.py
@@ -25,17 +25,17 @@ class ActorRole(Enum):
     the role of actor engaged in system
     for database config.db.user.role
     """
-    HUMAN_CUSTOMER = 0      # human being customer of the system
-    HUMAN_SERVICE = 1       # human being who provide customer service to consumer
-    AI_MACHINE_SERVICE = 2          # an AI role in system, such as LLM, etc.
+    HUMAN_CUSTOMER          = 0      # human being customer of the system
+    HUMAN_SERVICE_PROVIDER  = 1      # human being who provide customer service to consumer
+    AI_SERVICE_PROVIDER     = 2      # an AI role in system, such as LLM, etc.
 
 
 class AI_SERVICE_STATUS(Enum):
     """
     the status of AI service provided to human customers
     """
-    OPEN = 1
-    ClOSE = 0
+    OPEN    = 1
+    ClOSE   = 0
 
 
 if __name__ == "__main__":
