9e4d8a27fcfd180d6cc5dca9905f3c0edbe10bb3
richard <whoami@hotmail.com>
2025-05-06 17:20:13 +0800

    debug

--- vector_db_util.py
+++ vector_db_util.py
@@ -14,11 +14,15 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
 from os import cpu_count
 from os.path import exists
+import torch
 
 import logging.config
 import re
+
+from sys_init import init_yml_cfg
 
 embedding_model = "../bge-large-zh-v1.5"
 vector_db_dir = "./faiss_index"
@@ -27,12 +31,14 @@ logging.config.fileConfig('logging.conf', encoding="utf-8")
 logger = logging.getLogger(__name__)
 
 
-def process_doc(documents: list[Document], embedding: str, vector_db: str, chunk_size=500, chunk_overlap=50) -> None:
+def process_doc(documents: list[Document], embedding: str, vector_db: str,
+                sys_cfg: dict, chunk_size=500, chunk_overlap=50) -> None:
     """
     process a Document list object, a common tools
     :param documents: A Document list
     :param embedding: A embedding model name
     :param vector_db: A vector db dir name
+    :param sys_cfg: system configuration for model and API call
     :param chunk_size: A text chunk size of each batch
     :param chunk_overlap: text can be overlapped between chunks
     """
@@ -54,11 +60,23 @@ def process_doc(documents: list[Document], embedding: str, vector_db: str, chunk
         separators=['\n\n', '。', '！', '？', '；', '...']
     )
     texts = text_splitter.split_documents(cleaned_docs)
-    logger.info(f"load_embedding_model: {embedding}")
-    embeddings = HuggingFaceEmbeddings(
-        model_name=embedding,
-        cache_folder='./bge-cache',
-        model_kwargs={'device': 'cpu'}
+    # get local embeddings
+    # logger.info(f"load_embedding_model: {embedding}")
+    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # logger.info(f"available_device_for_embedding: {device}")
+    # embeddings = HuggingFaceEmbeddings(
+    #     model_name=embedding,
+    #     cache_folder='./bge-cache',
+    #     model_kwargs={'device': device}
+    # )
+    # get remote embeddings model
+    logger.info(f"ai_config: {sys_cfg}")
+    embeddings = OpenAIEmbeddings(
+        model=sys_cfg["embedding_model_name"],
+        tiktoken_model_name = sys_cfg["embedding_model_name"],
+        tiktoken_enable=False,
+        openai_api_base=f"{sys_cfg['api_uri']}/embeddings",
+        openai_api_key=sys_cfg["api_key"]
     )
     logger.info("build_vector_db")
     # if exists(vector_db):
@@ -109,7 +127,7 @@ def clean_line_breaks(text, line_width=30, threshold=0.8) -> str:
             cleaned.append(line + '\n')
     return ''.join(cleaned).replace('\n\n', '\n')
 
-def vector_txt(txt_file: str):
+def vector_txt(txt_file: str, sys_cfg: dict):
     """
     vector a txt file.
     :param txt_file: a text file full path
@@ -118,10 +136,10 @@ def vector_txt(txt_file: str):
     loader = TextLoader(txt_file, encoding='utf8')
     docs = loader.load()
     # process_doc(docs)
-    process_doc(docs, embedding_model, vector_db_dir)
+    process_doc(docs, embedding_model, vector_db_dir, sys_cfg)
 
 
-def vector_pdf_dir(pdf_dir: str):
+def vector_pdf_dir(pdf_dir: str, sys_cfg: dict):
     """
     :param pdf_dir: a directory with all pdf file
     """
@@ -134,9 +152,9 @@ def vector_pdf_dir(pdf_dir: str):
     )
     documents = loader.load()
     logger.info(f"loaded {len(documents)} documents")
-    process_doc(documents, embedding_model, vector_db_dir)
+    process_doc(documents, embedding_model, vector_db_dir, sys_cfg)
 
-def vector_txt_dir(pdf_dir: str):
+def vector_txt_dir(pdf_dir: str, sys_cfg: dict):
     """
     :param pdf_dir: a directory with all pdf file
     """
@@ -144,12 +162,15 @@ def vector_txt_dir(pdf_dir: str):
     # 加载知识库文件
     logger.info(f"load local file from {pdf_dir}")
     loader = DirectoryLoader(
-        path=pdf_dir, recursive=True, load_hidden=False,
-        loader_cls=TextLoader, glob="**/*.txt"
+        path=pdf_dir,
+        recursive=True,
+        load_hidden=False,
+        loader_cls=TextLoader,
+        glob="**/*.txt"
     )
     docs = loader.load()
     logger.info(f"loaded {len(docs)} documents")
-    process_doc(docs, embedding_model, vector_db_dir)
+    process_doc(docs, embedding_model, vector_db_dir, sys_cfg)
 
 
 def get_vector_db(db_dir: str) -> FAISS:
@@ -192,6 +213,7 @@ if __name__ == "__main__":
     # vector_txt("/home/rd/doc/文档生成/knowledge_base/1.txt")
     # vector_pdf_dir("/home/rd/doc/文档生成/knowledge_base")
     # vector_pdf("/home/rd/doc/文档生成/knowledge_base/1.pdf")
-    vector_txt_dir("/home/rd/doc/文档生成/knowledge_base")
+    my_cfg = init_yml_cfg()
+    vector_txt_dir("/home/rd/doc/文档生成/knowledge_base", my_cfg["ai"])
     result = search("分析本系统需遵循的国家合规性要求，包括但不限于网络安全法、等级保护要求、数据安全法，密码法，个人信息保护规范等", "faiss_index")
     logger.info(f"score:{result[0][1]}, \nsource_file:{result[0][0].metadata["source"]}, \ncontent: {result[0][0].page_content}")
