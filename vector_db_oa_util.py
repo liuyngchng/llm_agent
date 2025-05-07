6400abeda12f0e005cd075f8478622f26f1ad182
richard <whoami@hotmail.com>
2025-05-07 15:03:17 +0800

    debug

--- vector_db_oa_util.py
+++ vector_db_oa_util.py
@@ -6,6 +6,7 @@
 for OpenAI compatible remote API
 """
 import httpx
+import os
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
@@ -37,31 +38,39 @@ class RemoteEmbeddings(Embeddings):  # 适配器类
         return resp.data[0].embedding
 
 
-def process_doc(documents: list[Document], vector_db: str, sys_cfg:dict, chunk_size=500, chunk_overlap=50) -> None:
+def process_doc(documents: list[Document], vector_db: str, sys_cfg:dict,
+                chunk_size=15, chunk_overlap=2) -> None:
     logger.info(f"loaded {len(documents)} documents, files_name_list_as_following")
     logger.info("split_doc")
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap,
-        separators=['\n\n', '。', '！', '？', '；', '...']
+        separators=['\n', '。', '！', '？', '；', '...']
     )
     texts = text_splitter.split_documents(documents)
     logger.info(f"split_doc_finished, {texts}")
-    client = OpenAI(
+    client = build_client(sys_cfg)
+    logger.info(f"init_client_with_config: {sys_cfg}")
+    embeddings = RemoteEmbeddings(client)
+    vectorstore = FAISS.from_documents(texts, embeddings)
+    vectorstore.save_local(vector_db)
+    logger.info(f"save_vector_to_local {vector_db}")
+
+def build_client(sys_cfg: dict):
+    return OpenAI(
         base_url= sys_cfg['api_uri'],   # "https://myhost/v1",
         api_key= sys_cfg['api_key'],    # "sk-xxxxx",
         http_client=httpx.Client(verify=False),
     )
-    logger.info(f"init_client_with_config: {sys_cfg}")
-    txt_list = []
-    for doc in texts:
-        txt_list.append(doc.page_content)
-        response = client.embeddings.create(
-            model="bge-m3",
-            input=doc.page_content
-        )
-        logger.info(f"response {response}")
 
+def load_vector_db(vector_db: str, sys_cfg: dict):
+    client = build_client(sys_cfg)
+    embeddings = RemoteEmbeddings(client)
+    return FAISS.load_local(vector_db, embeddings, allow_dangerous_deserialization=True)
+
+def search_similar_text(query: str, vector_db, sys_cfg: dict, top_k=3):
+    db = load_vector_db(vector_db, sys_cfg)
+    return db.similarity_search_with_relevance_scores(query, k=top_k)
 
 
 def vector_txt(txt_file: str, sys_cfg:dict):
@@ -72,9 +81,10 @@ def vector_txt(txt_file: str, sys_cfg:dict):
 
 
 if __name__ == "__main__":
-
-    import os
-
-    os.environ["NO_PROXY"] = "petrotech.cnpc"  # 禁用代理
+    os.environ["NO_PROXY"] = "*"  # 禁用代理
     my_cfg = init_yml_cfg()
     vector_txt("./1.txt", my_cfg['ai'])
+
+    results = search_similar_text("奇怪的句子", vector_db_dir, my_cfg['ai'])
+    for result in results:
+        logger.info(f"search_result: {result[0].page_content}, score {result[1]}")
