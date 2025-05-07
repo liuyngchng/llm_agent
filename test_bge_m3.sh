097eb655aa363af69330957f1576c9ffafff5b4a
richard <whoami@hotmail.com>
2025-05-07 10:09:42 +0800

    debug

---
+++ test_bge_m3.sh
@@ -0,0 +1,9 @@
+curl -X POST "http://127.0.0.1:8000/v1/embed" \
+	-H "Content-Type: application/json" \
+	-d '{"model": "bge-m3", "text": "需要编码的文本"}'
+	
+	
+curl -ks --noproxy '*' -w'\n' --tlsv1 -X POST  'https://aiproxy.petrotech.cnpc/v1/embeddings' \
+	-H "Content-Type: application/json"\
+	-H 'Authorization: Bearer sk-8r****Fe'\
+	-d '{"model": "bge-m3","input": "本"}' | jq	
