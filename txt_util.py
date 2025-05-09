8665b0a077cc40e79d7772ae6cc8d5603c03fdeb
richard <whoami@hotmail.com>
2025-05-09 11:12:13 +0800

    debug

--- txt_util.py
+++ txt_util.py
@@ -8,20 +8,20 @@ import logging.config
 logging.config.fileConfig('logging.conf', encoding="utf-8")
 logger = logging.getLogger(__name__)
 
-def get_txt_in_dir_by_keywords(keywords: str, dir_path: str) -> str:
+def get_txt_in_dir_by_keywords(keywords: str, dir_path: str, word_count = 500) -> str:
     logger.info(f"get_txt_by_keywords({keywords}, {dir_path})")
     result = ""
     for filename in os.listdir(dir_path):
         if not filename.endswith('.txt'):
             continue
         file_full_path = os.path.join(dir_path, filename)
-        file_result = get_txt_in_file_by_keywords(keywords, file_full_path)
+        file_result = get_txt_in_file_by_keywords(keywords, file_full_path, word_count)
         result += f"\n\n[{file_full_path}]\n{file_result}"
     logger.info(f"get_txt_in_dir_by_keywords_return, {result}")
     return result
 
 
-def get_txt_in_file_by_keywords(keywords: str, file_name: str) -> str:
+def get_txt_in_file_by_keywords(keywords: str, file_name: str, word_count: int) -> str:
     with open(file_name, 'r', encoding='utf-8') as f:
         lines = [line for line in f]
         full_text = ''.join(lines)
@@ -30,7 +30,7 @@ def get_txt_in_file_by_keywords(keywords: str, file_name: str) -> str:
             clean_line = strip_prefix_no(raw_line)
             if keywords == clean_line:
                 pos = full_text.find(raw_line)
-                result.append(full_text[pos: pos + len(raw_line) + 1000])
+                result.append(full_text[pos: pos + len(raw_line) + word_count])
         return '\n'.join(result)
 
 def strip_prefix_no(txt: str) -> str:
