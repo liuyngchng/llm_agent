10092fa928cb45d676c20e41ab1427fa884fca78
richard <whoami@hotmail.com>
2025-05-13 09:50:45 +0800

    debug

--- pip_install_bare_metal.sh
+++ pip_install_bare_metal.sh
@@ -4,6 +4,9 @@ PROXY='-i http://devpi.11.11.77.81.nip.io/root/pypi/+simple --trusted-host devpi
 echo ${PROXY}
 pip install gunicorn \
     langgraph langchain_ollama langchain_openai langchain_community \
-    langchain langchain_huggingface langchain_text_splitters langchain_huggingface langchain_unstructured unstructured \
-    unstructured[pdf] langchain_core flask pydantic python-docx nltk sentence-transformers faiss_cpu torch \
-    concurrent_log_handler pymysql cx_Oracle sounddevice pydub pycryptodome wheel qrcode[pil] lxml tabulate ${PROXY}
+    langchain langchain_huggingface langchain_text_splitters \
+    langchain_huggingface langchain_unstructured unstructured \
+    unstructured[pdf] langchain_core flask pydantic python-docx \
+    nltk sentence-transformers faiss_cpu torch concurrent_log_handler \
+    pymysql cx_Oracle sounddevice pydub pycryptodome wheel qrcode[pil] \
+    lxml tabulate oracledb ${PROXY}
