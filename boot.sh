731a18b10548d3d64c047aba42d52c0574ed1468
richard <whoami@hotmail.com>
2025-06-06 13:27:45 +0800

    debug

--- boot.sh
+++ boot.sh
@@ -5,7 +5,7 @@ MODULE=http_nl2sql
 echo "current dir `pwd`"
 #nohup /opt/llm_py_env/bin/gunicorn --timeout 240 --preload -w 4 -b 0.0.0.0:19000 http_service:app > server.log &2>&1 &
 # just for simplicity to deploy and share memory
-/opt/llm_py_env/bin/gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
+#/opt/llm_py_env/bin/gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
 /opt/llm_py_env/bin/gunicorn --certfile ./cert/nl2sql.crt --keyfile ./cert/nl2sql.key --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
 # real production environment
 #/opt/llm_py_env/bin/gunicorn --timeout 240 --preload -w 4 -b 0.0.0.0:19000 ${MODULE}:app
