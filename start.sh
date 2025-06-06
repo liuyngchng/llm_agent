12e600fbe42af6257f727b67c28ff3bc754800cf
richard <whoami@hotmail.com>
2025-06-06 10:33:34 +0800

    debug

--- start.sh
+++ start.sh
@@ -10,4 +10,6 @@ echo 'starting module '${MODULE}
 # active the virtual env for python, or you can hardcode your env python location
 source ../llm_py_env/bin/activate
 # start your module with gunicorn WSGI
-gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
+#gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
+
+gunicorn --certfile ./cert/nl2sql.crt --keyfile ./cert/nl2sql.key --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
