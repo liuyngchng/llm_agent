9ef1c4f7a53b5e19689f5e3791126cb68cbe82b0
richard <whoami@hotmail.com>
2025-06-25 16:16:27 +0800

    debug

--- boot.sh
+++ boot.sh
@@ -1,7 +1,8 @@
 /bin/bash -c 'source ../llm_py_env/bin/activate'
 # for start application in docker container
-MODULE=http_stream
+#MODULE=http_stream
 #MODULE=http_rag
+MODULE=http_docx
 echo "current dir `pwd`"
 #nohup /opt/llm_py_env/bin/gunicorn --timeout 240 --preload -w 4 -b 0.0.0.0:19000 http_service:app > server.log &2>&1 &
 # just for simplicity to deploy and share memory
