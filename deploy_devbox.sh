bc81522b6ad29abf500b1473d20521c4d3969c53
richard <whoami@hotmail.com>
2025-06-27 15:17:03 +0800

    debug

--- deploy_devbox.sh
+++ deploy_devbox.sh
@@ -3,7 +3,7 @@
 sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 cd project
 rm *.py *.sh *.db *.log
-rm -fr resources/ templates/ static/ __pycache__/ cert/ faiss_index/  faiss_oa_vector/
+rm -fr resources/ templates/ static/ __pycache__/ cert/ faiss_index/  faiss_oa_vector/ hack/ sh/ upload_doc/
 cd ..
 tar -xf llm_agent.tar
 mv llm_agent/* project
