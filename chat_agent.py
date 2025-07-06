0ac4b131c7ebe5dec18863630782337852019a25
richard <liuyngchng@hotmail.com>
2025-07-06 10:14:59 +0800

    debug

---
+++ chat_agent.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging.config
+import httpx
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.chat_models import ChatOpenAI, ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+from pydantic import SecretStr
+
+logging.config.fileConfig('logging.conf', encoding="utf-8")
+logger = logging.getLogger(__name__)
+
+
+class ChatAgent:
+
+    def __init__(self, cfg:dict , is_remote_model=True, prompt_padding=""):
+        self.cfg = cfg
+        self.llm_api_uri = cfg['api']['llm_api_uri']
+        self.llm_api_key = SecretStr(cfg['api']['llm_api_key'])
+        self.llm_model_name = cfg['api']['llm_model_name']
+        self.is_remote_model = is_remote_model
+        self.llm = self.get_llm()
+
+    def get_llm(self):
+        if self.is_remote_model:
+            if "https" in self.llm_api_uri:
+                model = ChatOpenAI(
+                    api_key=self.llm_api_key,
+                    base_url=self.llm_api_uri,
+                    http_client=httpx.Client(verify=False, proxy=None),
+                    model=self.llm_model_name,
+                    temperature=0,
+                    streaming=True,
+                )
+            else:
+                model = ChatOllama(model=self.llm_model_name, base_url=self.llm_api_uri, temperature=0, disable_streaming= False,)
+        else:
+            model = ChatOllama(model=self.llm_model_name, base_url=self.llm_api_uri, temperature=0, disable_streaming= False,)
+        logger.debug(f"model, {model}")
+        return model
+
+    def get_chain(self):
+        template = """你是一个专业的燃气运营支持助手，请根据以下上下文信息回答问题：
+            {context}
+            
+            问题：{question}
+            """
+        prompt = ChatPromptTemplate.from_template(template)
+        model = self.get_llm()
+        chain = (
+                {"context": RunnablePassthrough(), "question": RunnablePassthrough()}
+                | prompt
+                | model
+                | StrOutputParser()
+        )
+        return chain
--- http_chat.py
+++ http_chat.py
@@ -13,10 +13,12 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask, request, redirect, jsonify, render_template, url_for
 
 import cfg_util as cfg_utl
+from chat_agent import ChatAgent
 from sys_init import init_yml_cfg
 from bp_auth import auth_bp, auth_info, get_client_ip
 from bp_vdb import file_vdb
 from vdb_util import search_txt
+
 
 logging.config.fileConfig('logging.conf', encoding="utf-8")
 logger = logging.getLogger(__name__)
@@ -55,14 +57,9 @@ def chat(catch=None):
     uid = request.form.get('uid').strip()
     session_key = f"{uid}_{get_client_ip()}"
     if not auth_info.get(session_key, None) or time.time() - auth_info.get(session_key) > SESSION_TIMEOUT:
-        waring_info = {
-            "chart": {},
-            "raw_dt": "登录信息已失效，请重新登录后再使用本系统", "sql": "",
-            "explain_sql": "", "total_count": 0, "cur_page": 1,
-            "total_page": 1
-        }
+        waring_info = "登录信息已失效，请重新登录后再使用本系统"
         logger.error(f"{waring_info}, {uid}")
-        return json.dumps(waring_info, ensure_ascii=False)
+        return waring_info
     logger.info(f"rcv_msg, {msg}, uid {uid}")
     auth_info[session_key] = time.time()
     my_vector_db_dir = f"upload_doc/faiss_oa_idx_{uid}"
@@ -70,14 +67,17 @@ def chat(catch=None):
     if not os.path.exists(my_vector_db_dir):  # 新增检查
         logger.info(f"vector_db_dir_not_exists_return_none, {my_vector_db_dir}")
         answer = "暂时没有相关知识提供给您，请您联系系统管理员"
+        return answer
     else:
-        search_result = search_txt(msg, my_vector_db_dir, 0.1, my_cfg['api'], 3)
-        # TODO: 以搜索到的文本内容作为RAG输入，请求大模型接口（兼容OpenAI方式）， 提供流式输出
-        answer = ""
-
-    # str_info = json.dumps(answer, ensure_ascii=False)
-    logger.info(f"return {answer}")
-    return answer
+        context = search_txt(msg, my_vector_db_dir, 0.1, my_cfg['api'], 3)
+        chat_agent = ChatAgent(my_cfg)
+        def generate_stream():
+            full_response = ""
+            for chunk in chat_agent.get_chain().stream({"context": context, "question": msg}):
+                full_response += chunk
+                yield chunk
+            logger.info(f"full_response: {full_response}")
+        return app.response_class(generate_stream(), mimetype='text/event-stream')
 
 
 @app.route('/vdb/idx', methods=['GET'])
