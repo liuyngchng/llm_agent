b35b6fa9c12c5a9cedf9d7a5813052b1fa6d6622
richard <whoami@hotmail.com>
2025-07-10 17:15:05 +0800

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # for start application in dev box
 source ./bin/activate
-gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 http_rag:app
+gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 http_docx:app
