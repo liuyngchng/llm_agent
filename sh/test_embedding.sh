c0da71935ca79cfea00f329bd5aa21889a77608b
richard <whoami@hotmail.com>
2025-08-13 10:43:48 +0800

    debug

--- sh/test_embedding.sh
+++ sh/test_embedding.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-curl -X POST "http://127.0.0.1:8000/v1/embed" \
-	-H "Content-Type: application/json" \
-	-d '{"model": "bge-m3", "text": "需要编码的文本"}'
-	
-	
+TOKEN=$(head -n 1 llm_token.txt)
 curl -ks --noproxy '*' -w'\n' --tlsv1 -X POST  'https://aiproxy.petrotech.cnpc/v1/embeddings' \
-	-H "Content-Type: application/json"\
-	-H 'Authorization: Bearer sk-8r****Fe'\
-	-d '{"model": "bge-m3","input": "本"}' | jq	
+    -H "Content-Type: application/json" \
+	  -H "Authorization: Bearer ${TOKEN}" \
+    -d '{"model": "bge-m3","input": "本"}' | jq
