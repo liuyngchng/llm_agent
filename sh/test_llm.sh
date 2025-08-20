d4312af7ee236dbcf27e640f233db36d252570b5
richard <whoami@hotmail.com>
2025-08-20 11:14:44 +0800

    debug

--- sh/test_llm.sh
+++ sh/test_llm.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-API='https://aiproxy.petrotech.cnpc/v1/chat/completions'
-TOKEN=$(head -n 1 llm_token.txt)
-#MODEL='deepseek-v3'  # 可切换其他模型测试
-#MODEL='deepseek-r1'
-MODEL='qwen2dot5-7b-chat'
-#MODEL='qwen2dot5-72b-chat'
-#MODEL='kunlunllm-13b'
+API=$(sed -n '1p' llm_token.txt)
+TOKEN=$(sed -n '2p' llm_token.txt)
+MODEL=$(sed -n '3p' llm_token.txt)
+echo "API: ${API}"
 echo "TOKEN: ${TOKEN}"
 echo "MODEL: ${MODEL}"
 # 其中的 tools 部分对于支持 tools 函数调用的模型是必须的
--- sh/test_ocr.sh
+++ sh/test_ocr.sh
@@ -4,14 +4,15 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 #your llm config file, api_uri in first line and api_token in second line
-config_file="llm.txt"
+config_file="llm_token.txt"
 if [ ! -f "${config_file}" ]; then
     echo "错误：LLM 配置文件 $config_file 不存在"
     exit 1
 fi
 api_uri=$(sed -n '1p' "$config_file")
 api_token=$(sed -n '2p' "$config_file")
-echo "api_uri=${api_uri}, api_token=${api_token}"
+model_name="qwen2-7b-vl"
+echo "api_uri=${api_uri}, api_token=${api_token}, model_name=${model_name}"
 image_path="$1"
 echo "image_path=${image_path}"
 mime_type="image/$(file -b --mime-type "$image_path" | cut -d'/' -f2)"
@@ -22,7 +23,7 @@ tmpfile=$(mktemp)
 
 cat <<EOF > "$tmpfile"
 {
-  "model": "qwen2-7b-vl",
+  "model": "${model_name}",
   "messages": [
     {
       "role": "user",
