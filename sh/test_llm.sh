f5fe7c3368bbcaf5ab9fe9dde816470fc2d19378
rd-ext <liuyngch@hotmail.com>
2025-09-30 14:44:30 +0800

    debug

--- sh/test_llm.sh
+++ sh/test_llm.sh
@@ -5,7 +5,8 @@ MODEL=$(sed -n '3p' llm_token.txt)
 echo "API: ${API}"
 echo "TOKEN: ${TOKEN}"
 echo "MODEL: ${MODEL}"
-# 其中的 tools 部分对于支持 tools 函数调用的模型是必须的
+
+# chat with llm
 curl -ks --noproxy '*' \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer ${TOKEN}" \
