226b7e3c1bfb0cd31f06d1b7ab60cea38a45456c
richard <liuyngchng@hotmail.com>
2025-10-30 09:38:38 +0800

    debug

--- common/cfg_db_schema/statistics.sql
+++ common/cfg_db_schema/statistics.sql
@@ -1,8 +1,8 @@
 -- 用户每日访问量，以及输入输出Token数量统计明细表
 CREATE TABLE "statistics" (
 	"id"	INTEGER NOT NULL UNIQUE,
-	"uid"	INTEGER NOT NULL UNIQUE,
-	"nickname"    TEXT NOT NULL,
+	"uid"	INTEGER NOT NULL,
+	"nickname"	TEXT NOT NULL,
 	"date"	TEXT NOT NULL,
 	"access_count"	INTEGER NOT NULL DEFAULT 0,
 	"input_token"	INTEGER NOT NULL DEFAULT 0,
