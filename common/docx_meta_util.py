bae8e53297126d321cda812cb5f5e811ac456b37
rd-ex <liuyngchng@hotmail.com>
2025-11-22 16:28:57 +0800

    debug

--- common/docx_meta_util.py
+++ common/docx_meta_util.py
@@ -15,7 +15,7 @@ from common.my_enums import DataType
 logging.config.fileConfig('logging.conf', encoding="utf-8")
 logger = logging.getLogger(__name__)
 
-def save_docx_file_info(uid: int, task_id: int, doc_type: str, doc_title: str, doc_outline:str,
+def save_doc_file_info(uid: int, task_id: int, doc_type: str, doc_title: str, doc_outline:str,
                         keywords: str, input_file_path: str,  vdb_id: int, is_include_para_txt: int) -> dict:
     """
     保存docx文件处理任务的相关元数据信息
@@ -30,11 +30,11 @@ def save_docx_file_info(uid: int, task_id: int, doc_type: str, doc_title: str, d
     :param is_include_para_txt: 写作的 Word 文档模板中是否包含有文本段落
     :return:
     """
-    logger.debug(f"save_docx_file_info, {uid}, {task_id}, {doc_type}, {doc_title}, {keywords}, {input_file_path}")
+    logger.debug(f"save_doc_file_info, {uid}, {task_id}, {doc_type}, {doc_title}, {keywords}, {input_file_path}")
     timestamp = time.time()
     # 生成类似格式（UTC时间）
     create_time = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(timestamp))
-    sql = (f"insert into docx_file_info(uid, task_id, doc_type, doc_title, doc_outline, "
+    sql = (f"insert into doc_file_info(uid, task_id, doc_type, doc_title, doc_outline, "
            f"keywords, input_file_path, vdb_id, is_include_para_txt, create_time) values "
            f"({uid}, {task_id}, '{doc_type}', '{doc_title}', '{doc_outline}',"
            f"'{keywords}', '{input_file_path}', {vdb_id}, {is_include_para_txt}, '{create_time}')")
@@ -43,7 +43,7 @@ def save_docx_file_info(uid: int, task_id: int, doc_type: str, doc_title: str, d
         my_dt = insert_del_sqlite(my_conn, sql)
         return my_dt
 
-def get_docx_file_info(task_id: int) -> dict:
+def get_doc_file_info(task_id: int) -> dict:
     """
     根据任务id获取docx文件处理任务的相关元数据信息
     :param task_id: process task id
@@ -51,7 +51,7 @@ def get_docx_file_info(task_id: int) -> dict:
     """
     if not task_id:
         raise RuntimeError(f"param_null_err {task_id}")
-    sql = f"select * from docx_file_info where task_id = {task_id} limit 1"
+    sql = f"select * from doc_file_info where task_id = {task_id} limit 1"
     logger.info(f"get_docx_info_by_task_id_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
     logger.info(f"get_docx_info_by_task_id_dt {my_dt}")
@@ -65,7 +65,7 @@ def get_user_task_list(uid: int) -> dict:
     """
     if not uid:
         raise RuntimeError(f"param_uid_null_err {uid}")
-    sql = f"select * from docx_file_info where uid = {uid} order by task_id desc limit 100"
+    sql = f"select * from doc_file_info where uid = {uid} order by task_id desc limit 100"
     # logger.info(f"get_user_docx_task_list_by_uid_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
     # logger.info(f"get_user_docx_task_list_by_uid_dt {my_dt}")
@@ -75,7 +75,7 @@ def get_processing_file_list()-> list:
     """
     获取需要处理的docx任务信息清单
     """
-    sql = f"select * from docx_file_info where percent != 100 limit 100"
+    sql = f"select * from doc_file_info where percent != 100 limit 100"
     # logger.info(f"get_docx_processing_file_list_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
     # logger.info(f"get_docx_processing_file_list_dt {my_dt}")
@@ -87,7 +87,7 @@ def delete_task(task_id: int):
     :param task_id: process task id
     :return:
     """
-    sql = f"delete from docx_file_info where task_id ={task_id} limit 1 "
+    sql = f"delete from doc_file_info where task_id ={task_id} limit 1 "
     with sqlite3.connect(CFG_DB_FILE) as my_conn:
         logger.info(f"delete_docx_info_by_task_id_sql, {sql}")
         my_dt = insert_del_sqlite(my_conn, sql)
@@ -104,10 +104,10 @@ def save_write_doc_ctx(uid: int, task_id: int, doc_ctx: str):
     """
     if not uid or not task_id or not doc_ctx:
         raise RuntimeError(f"{uid}, param_null_err, {task_id}, {doc_ctx}")
-    sql = f"update docx_file_info set doc_ctx = '{doc_ctx}' where task_id = {task_id} limit 1"
-    logger.debug(f"{uid}, update_docx_file_info_docx_ctx_sql, {sql}")
+    sql = f"update doc_file_info set doc_ctx = '{doc_ctx}' where task_id = {task_id} limit 1"
+    logger.debug(f"{uid}, update_doc_file_info_docx_ctx_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
-    logger.debug(f"{uid}, update_docx_file_info_docx_ctx_dt, {my_dt}")
+    logger.debug(f"{uid}, update_doc_file_info_docx_ctx_dt, {my_dt}")
     return my_dt
 
 def save_output_doc_path(uid: int, task_id: int, output_file_path: str):
@@ -120,10 +120,10 @@ def save_output_doc_path(uid: int, task_id: int, output_file_path: str):
     """
     if not uid or not task_id or not output_file_path:
         raise RuntimeError(f"{uid}, param_null_err, {task_id}, {output_file_path}")
-    sql = f"update docx_file_info set output_file_path = '{output_file_path}' where task_id = {task_id} limit 1"
-    logger.debug(f"{uid}, update_docx_file_info_docx_output_doc_path_sql, {sql}")
+    sql = f"update doc_file_info set output_file_path = '{output_file_path}' where task_id = {task_id} limit 1"
+    logger.debug(f"{uid}, update_doc_file_info_docx_output_doc_path_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
-    logger.debug(f"{uid}, update_docx_file_info_docx_output_file_path_dt, {my_dt}")
+    logger.debug(f"{uid}, update_doc_file_info_docx_output_file_path_dt, {my_dt}")
     return my_dt
 
 def save_write_doc_vdb_dir(uid: int, task_id: int, vdb_dir: str):
@@ -136,10 +136,10 @@ def save_write_doc_vdb_dir(uid: int, task_id: int, vdb_dir: str):
     """
     if not uid or not task_id or not vdb_dir:
         raise RuntimeError(f"{uid}, param_null_err, {task_id}, {vdb_dir}")
-    sql = f"update docx_file_info set vdb_dir = '{vdb_dir}' where task_id = {task_id} limit 1"
-    logger.debug(f"{uid}, update_docx_file_info_docx_vdb_dir_sql, {sql}")
+    sql = f"update doc_file_info set vdb_dir = '{vdb_dir}' where task_id = {task_id} limit 1"
+    logger.debug(f"{uid}, update_doc_file_info_docx_vdb_dir_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
-    logger.debug(f"{uid}, update_docx_file_info_docx_vdb_dir_dt, {my_dt}")
+    logger.debug(f"{uid}, update_doc_file_info_docx_vdb_dir_dt, {my_dt}")
     return my_dt
 
 
@@ -155,33 +155,33 @@ def update_process_info(uid: int, task_id: int, process_info: str, percent = -1)
     if not task_id or not process_info:
         raise RuntimeError(f"{uid}, param_null_err, {task_id}, {process_info}")
     if percent == -1:
-        sql = f"update docx_file_info set process_info = '{process_info}' where uid = {uid} and task_id = {task_id} limit 1"
+        sql = f"update doc_file_info set process_info = '{process_info}' where uid = {uid} and task_id = {task_id} limit 1"
     else:
-        sql = f"update docx_file_info set process_info = '{process_info}', percent= {percent} where uid = {uid} and task_id = {task_id} limit 1"
-    logger.info(f"{uid}, update_docx_file_info_sql, {sql}")
+        sql = f"update doc_file_info set process_info = '{process_info}', percent= {percent} where uid = {uid} and task_id = {task_id} limit 1"
+    logger.info(f"{uid}, update_doc_file_info_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
-    logger.info(f"{uid}, update_docx_file_info_dt, {my_dt}")
+    logger.info(f"{uid}, update_doc_file_info_dt, {my_dt}")
     return my_dt
 
 def set_doc_info_para_task_created_flag(uid: int, task_id: int):
     """
-    在 docx_file_info 中记录 段落生成任务已创建，置位标记位
+    在 doc_file_info 中记录 段落生成任务已创建，置位标记位
     :param uid: user id
     :param task_id: process task idt
     :return:
     """
     if not task_id:
         raise RuntimeError(f"{uid}, param_null_err, {task_id}")
-    sql = f"update docx_file_info set is_para_task_created = 1 where task_id = {task_id} limit 1"
-    logger.info(f"{uid}, update_docx_file_info_sql, {sql}")
+    sql = f"update doc_file_info set is_para_task_created = 1 where task_id = {task_id} limit 1"
+    logger.info(f"{uid}, update_doc_file_info_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
-    logger.info(f"{uid}, update_docx_file_info_dt, {my_dt}")
+    logger.info(f"{uid}, update_doc_file_info_dt, {my_dt}")
     return my_dt
 
 def save_para_task(uid: int, task_id: int, tasks: list):
     """
     保存用户文档生成任务的各个段落生成的子任务清单,
-    一个文档写作任务的任务ID task_id 对应多个 docx_para_info 记录
+    一个文档写作任务的任务ID task_id 对应多个 doc_para_info 记录
     :param uid: user id
     :param task_id: process task id
     :param tasks: 单个文档生成任务的段落生成任务清单
@@ -217,16 +217,16 @@ def save_para_task(uid: int, task_id: int, tasks: list):
         else:
             my_values = value_item
 
-    sql = (f"insert into docx_para_info (uid, task_id, para_id, heading, unique_key, "
+    sql = (f"insert into doc_para_info (uid, task_id, para_id, heading, unique_key, "
            f"para_text, user_comment, current_sub_title, namespaces, create_time) values {my_values}")
-    logger.debug(f"save_docx_para_info_sql, {sql}")
+    logger.debug(f"save_doc_para_info_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
-    logger.debug(f"save_docx_para_info_dt, {my_dt}")
+    logger.debug(f"save_doc_para_info_dt, {my_dt}")
     return my_dt
 
 def update_para_info(task_id: int, para_id: int, gen_txt: str, word_count: int, contains_mermaid: int):
     """
-    保存用户文档生成任务的系统子任务清单,,一个文档写作任务的任务ID task_id 对应多个 docx_para_info 记录
+    保存用户文档生成任务的系统子任务清单,,一个文档写作任务的任务ID task_id 对应多个 doc_para_info 记录
     :param task_id: process task id
     :param para_id: 文档段落 ID
     :param gen_txt: 当前段落生成的文本，后续将会将文本插入  para_id 之后
@@ -236,11 +236,11 @@ def update_para_info(task_id: int, para_id: int, gen_txt: str, word_count: int, 
     """
     if not task_id or not para_id or not gen_txt:
         raise RuntimeError(f"param_null_err, {task_id}, {para_id}, {gen_txt}")
-    sql = (f"update docx_para_info set gen_txt='{gen_txt}', word_count={word_count}, "
+    sql = (f"update doc_para_info set gen_txt='{gen_txt}', word_count={word_count}, "
        f"contains_mermaid={contains_mermaid}, status=1 where task_id={task_id} and para_id = {para_id} limit 1")
-    logger.info(f"update_docx_para_info_sql, {sql}")
+    logger.info(f"update_doc_para_info_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
-    logger.info(f"update_docx_para_info_dt, {my_dt}")
+    logger.info(f"update_doc_para_info_dt, {my_dt}")
     return my_dt
 
 def get_para_info(task_id: int, para_id: int=-1)-> list:
@@ -253,9 +253,9 @@ def get_para_info(task_id: int, para_id: int=-1)-> list:
     if not task_id:
         raise RuntimeError(f"param_null_err, {task_id}")
     if para_id == -1:
-        sql = f"select * from docx_para_info where task_id = {task_id}"
+        sql = f"select * from doc_para_info where task_id = {task_id}"
     else:
-        sql = f"select * from docx_para_info where task_id = {task_id} and para_id = {para_id} limit 1"
+        sql = f"select * from doc_para_info where task_id = {task_id} and para_id = {para_id} limit 1"
     logger.info(f"get_para_info_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
     logger.info(f"get_para_info_dt, {my_dt}")
@@ -271,7 +271,7 @@ def get_para_list_with_status(task_id: int, status: int, is_order_by_para_id_des
     """
     if not task_id:
         raise RuntimeError(f"param_null_err, {task_id}")
-    base_sql = f"select * from docx_para_info where task_id = {task_id} and status = {status}"
+    base_sql = f"select * from doc_para_info where task_id = {task_id} and status = {status}"
     if is_order_by_para_id_desc:
         sql = f"{base_sql} order by para_id desc"
     else:
@@ -289,7 +289,7 @@ def count_para_task(task_id: int)-> list:
     """
     if not task_id:
         raise RuntimeError(f"param_null_err, {task_id}")
-    sql = f"select count(1) from docx_para_info where task_id = {task_id}"
+    sql = f"select count(1) from doc_para_info where task_id = {task_id}"
     logger.info(f"get_para_info_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
     logger.info(f"get_para_info_dt, {my_dt}")
@@ -303,7 +303,7 @@ def count_mermaid_para(task_id: int)-> list:
     """
     if not task_id:
         raise RuntimeError(f"param_null_err, {task_id}")
-    sql = f"select count(1) from docx_para_info where task_id = {task_id} and contains_mermaid = 1"
+    sql = f"select count(1) from doc_para_info where task_id = {task_id} and contains_mermaid = 1"
     logger.info(f"get_para_info_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
     logger.info(f"get_para_info_dt, {my_dt}")
@@ -318,7 +318,7 @@ def update_gen_txt_count_by_task_id(task_id: int, word_count: int):
     """
     if not task_id or not word_count:
         raise RuntimeError(f"param_null_err, {task_id}, {word_count}")
-    sql = f"update docx_file_info set word_count = {word_count} where task_id = {task_id} limit 1"
+    sql = f"update doc_file_info set word_count = {word_count} where task_id = {task_id} limit 1"
     logger.info(f"update_docx_gen_txt_count_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
     logger.info(f"update_docx_gen_txt_count_dt {my_dt}")
@@ -329,7 +329,7 @@ def save_outline_by_task_id(task_id: int, outline: str):
     """
     if not task_id or not outline:
         raise RuntimeError(f"param_null_err, {task_id}, {outline}")
-    sql = f"update docx_file_info set outline = '{outline}' where task_id = {task_id} limit 1"
+    sql = f"update doc_file_info set outline = '{outline}' where task_id = {task_id} limit 1"
     logger.info(f"update_docx_outline_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
     logger.info(f"update_docx_outline_dt {my_dt}")
@@ -341,7 +341,7 @@ def update_img_count_by_task_id(task_id: int, img_count: int):
     """
     if not task_id or not img_count:
         raise RuntimeError(f"param_null_err, {task_id}, {img_count}")
-    sql = f"update docx_file_info set img_count = {img_count} where task_id = {task_id} limit 1"
+    sql = f"update doc_file_info set img_count = {img_count} where task_id = {task_id} limit 1"
     logger.info(f"update_img_count_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
     logger.info(f"update_img_count_dt {my_dt}")
@@ -351,7 +351,7 @@ def get_img_count_by_task_id(task_id: int)-> int:
     """
     获取需要处理的docx任务中的图片数量
     """
-    sql = f"select img_count from docx_file_info where task_id = {task_id} limit 1"
+    sql = f"select img_count from doc_file_info where task_id = {task_id} limit 1"
     logger.info(f"get_img_count_sql, {sql}")
     my_dt = sqlite_output(CFG_DB_URI, sql, DataType.JSON.value)
     logger.info(f"get_img_count_dt, {my_dt}")
