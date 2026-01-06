bd502020ac88f5a498b5b0c979c545e3fc928ff9
richard <liuyngchng@hotmail.com>
2026-01-06 16:59:12 +0800

    debug

--- apps/chat/deploy/deploy_chat.sh
+++ apps/chat/deploy/deploy_chat.sh
@@ -10,6 +10,7 @@ if [ ! -d ${APP_DIR} ]; then
     exit 1
 fi
 echo "当前目录为 ${CURRENT_DIR}, 正在复制 ${APP} 服务配置文件"
+cp ${APP_DIR}/.env ./
 cp ${APP_DIR}/cfg.db ./
 cp ${APP_DIR}/cfg.yml ./
 echo "正在部署 ${APP} 服务"
