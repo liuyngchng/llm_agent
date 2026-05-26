738154353a3ff82674cff84486b69769aa656547
richard <liuyngchng@hotmail.com>
2026-05-26 16:39:59 +0800

    debug

--- apps/auth_service/app.py
+++ apps/auth_service/app.py
@@ -363,7 +363,7 @@ if __name__ == "__main__":
     import uvicorn
 
     logger.info(f"my_cfg {my_cfg.get('db')},\n{my_cfg.get('api')}")
-    # port = get_console_arg1()
-    port = 19011
+    port = get_console_arg1()
+    # port = 19011
     logger.info(f"auth_service_listen_on_port {port}")
     uvicorn.run(app, host="0.0.0.0", port=port)
