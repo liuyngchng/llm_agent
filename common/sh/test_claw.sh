15939362229f78577f1fb203ff6e01038bc9405a
richard <liuyngchng@hotmail.com>
2026-05-28 16:20:20 +0800

    debug

--- common/sh/test_claw.sh
+++ common/sh/test_claw.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+mkdir -p /data/claw/workspace
+docker stop my_claw
+docker rm my_claw
+
 # 运行容器
 docker run -dit \
+  --security-opt seccomp=unconfined 	\
+	--security-opt apparmor=unconfined 	\
+	--privileged \
   --name my_claw \
-  -p 19001:18789 \
-  -v /data/claw/workspace:/root/.openclaw/workspace \
+  -p 18789:18789 \
+  -v /data/claw:/root/.openclaw \
   openclaw:1.0
 
 
