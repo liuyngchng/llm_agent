0930f37422d4803e395eae23bdf28d63b474be35
richard <liuyngchng@hotmail.com>
2026-06-12 10:04:40 +0800

    debug

--- apps/api_adapter/deploy/linux/deploy_claude_web.sh
+++ apps/api_adapter/deploy/linux/deploy_claude_web.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # 需要在容器内启动 api_adaptor,
-# /opt/claude_code/llm_agent
+# cd /opt/claude_code/llm_agent
 # python -m apps.api_adapter.app &
+APP="claude_code_web"
+CONTAINER="claude_code_web"
+echo "开始部署 ${APP} 服务"
+docker stop ${CONTAINER}
+echo "正在删除 ${APP} 服务"
+docker rm ${CONTAINER}
+echo "开始创建 ${CONTAINER} 容器"
 docker run -dit \
-	--name my_claude_code \
+	--name ${CONTAINER} \
 	--rm \
-	-v /data/remote/workspace:/opt/workspace \
+	-v /data/remote/workspace:/root/workspace \
 	-v /data/claude_code:/opt/claude_code \
 	-w /opt/workspace \
 	-e NODE_TLS_REJECT_UNAUTHORIZED=0 \
@@ -22,3 +29,8 @@ docker run -dit \
 	-p 19004:8765 \
 	my_claude_code:1.0 \
 	/opt/llm_py_env/bin/claude-web --host 0.0.0.0
+
+echo "容器 ${CONTAINER} 已启动"
+docker ps -a  | grep ${CONTAINER} --color=always
+echo "部署完成"
+docker logs -f ${CONTAINER}
